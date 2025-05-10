879fac71497fcdf63ae48db3aadfac7658f49f03
vpc20 <cuevasvp@gmail.com>
2025-05-11 07:01:33 +0800

    aoc 2022

--- aoc_5_part1a.py
+++ aoc_5_part1a.py
@@ -1,5 +1,5 @@
 import re
-from collections import deque
+
 
 def read_input_to_sections_no_strip(in_file):
     f = open(in_file)
@@ -21,8 +21,7 @@ if __name__ == '__main__':
     movement_data = sections[1]
 
     txt_arr = [e for e in stacks_data.split('\n')]
-    nums = re.findall(r'\d+', txt_arr[len(txt_arr) - 1])  # last line - stack number - 1  2  3 ...
-    stack_count = int(nums[-1])
+    stack_count = (len(txt_arr[-1]) + 2) // 4
     print('stack_count', stack_count)
 
     # 0   1
@@ -31,24 +30,23 @@ if __name__ == '__main__':
     # 3  13
 
     # create stacks
-    stacks = [deque() for i in range(stack_count)]
+    stacks = [[] for i in range(stack_count)]
     print(stacks)
-    for txt in txt_arr:
+    for txt in reversed(txt_arr[:-1]):
         txt = pad_trailing_spaces(txt, stack_count * 4 - 1)
-        if txt[1] != '1':  # ignore last line as these are just stack numbers
-            for i in range(stack_count):
-                idx = i * 4 + 1
-                if txt[idx] != ' ':
-                    stacks[i].appendleft(txt[idx])
+        for i in range(stack_count):
+            idx = i * 4 + 1
+            if txt[idx] != ' ':
+                stacks[i].append(txt[idx])
     print(stacks)
 
     # move stacks
-    for e in sections[1].split('\n'):
+    for e in movement_data.split('\n'):
         nums = re.findall(r'\d+', e)
         nums = [int(num) for num in nums]
         crate_count, from_stack, to_stack = nums
         for _ in range(crate_count):
-            stacks[to_stack-1].append(stacks[from_stack-1].pop())
+            stacks[to_stack - 1].append(stacks[from_stack - 1].pop())
     print(stacks)
 
     result = ''.join([e[-1] for e in stacks])  # get crates on top of stack
